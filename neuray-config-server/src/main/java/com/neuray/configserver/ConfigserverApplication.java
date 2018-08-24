1be149c231a453a13ddb85073c0a6633a75f9204
于海慧 <125227112@qq.com>
2018-08-24 16:19:08 +0800

    init

---
+++ neuray-config-server/src/main/java/com/neuray/configserver/ConfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.neuray.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+}
